bce8fcfd2df8f4d3f7f7926347736ce4fd15667e
Junejie Ruzol <junejie.ruzol@zumtobelgroup.com>
2017-05-19 14:37:24 +0800

    init

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Updating Squid Config"
+sed -i '/acl localnet src 172.16.0.0\/12/s/^#//g' /etc/squid/squid.conf
+sed -i '/http_access allow localnet/s/^#//g' /etc/squid/squid.conf
+
+echo "${PASSWORD}" | /usr/sbin/openconnect ${OPTIONS} ${SERVER}
+
+service squid start
+while true; do sleep 1000; done
