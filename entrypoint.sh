e5cc0f63a4559ab058a47c1d595eb35f26aee9ec
Junejie Ruzol <junejie.ruzol@zumtobelgroup.com>
2017-07-19 19:33:37 +0800

    add 8443 in supported port

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ echo "Updating Squid Config"
 sed -i '/acl localnet src 172.16.0.0\/12/s/^#//g' /etc/squid/squid.conf
 sed -i '/http_access allow localnet/s/^#//g' /etc/squid/squid.conf
 
+# support 8443
+sed -i '/443/a acl SSL_ports port 8443' /etc/squid/squid.conf
+
 echo "${PASSWORD}" | openconnect ${OPTIONS} ${SERVER} && \
 service squid start && \
 echo "Success"
